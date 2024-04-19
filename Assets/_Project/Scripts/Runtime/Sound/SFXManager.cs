4fc568d888c66db6954731b54b17816a6a2169a8
Chevalier <Adam@levett.biz>
2024-04-19 16:34:19 +0200

    stuff

---
+++ Assets/_Project/Scripts/Runtime/Sound/SFXManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SFXManager : MonoBehaviour
+{
+    #region FIELDS
+
+    public static SFXManager Instance;
+    public AudioClip[] EnemyDamageSound;
+    public AudioClip[] EnemyAttackSound;
+    private AudioSource audioSource;
+
+    #endregion FIELDS
+
+    #region UNITY METHODS
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    #endregion UNITY METHODS
+
+    #region METHODS
+
+    public void PlaySFX(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+    public void playEnemyDamage()
+    {
+        int rand = Random.Range(0, EnemyDamageSound.Length);
+        audioSource.PlayOneShot(EnemyDamageSound[rand]);
+    }
+
+    public void playEnemyAttack()
+    {
+        int rand = Random.Range(0, EnemyAttackSound.Length);
+        audioSource.PlayOneShot(EnemyAttackSound[rand]);
+    }
+
+    #endregion METHODS
+}
